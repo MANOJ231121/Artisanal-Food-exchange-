de57a660e11bf929216799144f12b87fc09fedfc
Manoj <bhawanajoshi979@gmail.com>
2026-07-07 12:45:28 +0530

    Delete Artisanal Food exchange directory

--- Artisanal Food exchange/src/main/java/com/example/artisanalfood/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.artisanalfood.repository;
-
-import com.example.artisanalfood.model.Review;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface ReviewRepository extends MongoRepository<Review, String> {
-    List<Review> findByProductId(String productId);
-}
--- Artisanal Food exchange/src/main/java/com/example/artisanalfood/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.artisanalfood.repository;
-
-import com.example.artisanalfood.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    List<User> findByRole(User.Role role);
-}
